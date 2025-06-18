ebcba2413050a71c57187806c239b1adafd9b5a4
ImaksI1 <maksvijtovic98@email.com>
2025-06-18 20:02:22 +0200

    added config support

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import logging
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram import Bot
+import configparser
 
-bot = Bot("8099195869:AAGqNU85844BOFyNMsfoUgdD6W-Z8b5QMDw")
-dp = Dispatcher()
-
-@dp.message(CommandStart())
-async def start_handler(message: Message):
-    gifts = await bot.get_available_gifts()
-    for gift in gifts:
-        name, gift_list = gift
-        print("Name:", name)
-        for g in gift_list:
-            print(f"Gift ID: {g.id}")
-            print(f"Gift Count: {g.remaining_count}")
-            if g.sticker.type == 'custom_emoji':
-                await message.answer(g.sticker.emoji)
-            else:
-                await bot.send_sticker(chat_id=message.from_user.id, sticker=g.sticker.file_id)
+config = configparser.ConfigParser()
+config.read("config.ini")
+bot = Bot(config["DEFAULT"]["BOT_TOKEN"])
 
 async def updater():
+    new_gifts_ids = []
     while True:
         gifts = await bot.get_available_gifts()
         for gift in gifts:
@@ -30,18 +17,17 @@ async def updater():
             for g in gift_list:
                 print(f"Gift ID: {g.id}")
                 print(f"Gift Count: {g.remaining_count}")
-                if g.remaining_count != None:
+                if g.remaining_count != None and g.id not in new_gifts_ids:
+                    new_gifts_ids.append(g.id)
                     if g.sticker.type == 'custom_emoji':
-                        await bot.send_message(chat_id=-1002721617802, text=g.sticker.emoji)
+                        await bot.send_message(chat_id=config["DEFAULT"]["CHAT_ID"], text=g.sticker.emoji)
                     else:
-                        await bot.send_sticker(chat_id=-1002721617802, sticker=g.sticker.file_id)
-        await asyncio.sleep(15)
-
+                        await bot.send_sticker(chat_id=config["DEFAULT"]["CHAT_ID"], sticker=g.sticker.file_id)
+        await asyncio.sleep(int(config["DEFAULT"]["UPDATER_INTERVAL"]))
 
 async def main():
-    asyncio.create_task(updater())
-    await dp.start_polling(bot)
-
+    await asyncio.create_task(updater())
+    await asyncio.Event().wait()
 
 if "__main__" == __name__:
     logging.basicConfig(level=logging.INFO)
